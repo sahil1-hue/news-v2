1323e447790ffdb90bade2f66e22d2b89250d48f
sahil1-hue <79180872+sahil1-hue@users.noreply.github.com>
2021-03-21 12:02:26 -0700

    url

---
+++ cs490Pro/news/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+
+# Create your views here.
+def newsPaper(request):
+    newsapi = NewsApiClient(api_key='573dab4634604cb0a5bc4a55de0f9e50')
+    top = newsapi.get_top_headlines(sources='new-scientist')
+
+    articles = top['articles']
+    description = []
+    newsTitle = []
+    img = []
+    url = []
+
+    for i in range(len(articles)):
+        newsInfo = articles[i]
+        newsTitle.append(newsInfo['title'])
+        description.append(newsInfo['description'])
+        img.append(newsInfo['urlToImage'])
+        url.append(newsInfo['url'])
+
+    Newslist = zip(newsTitle, description, url,img)
+
+    return render(request, 'index.html', context={"Newslist": Newslist})
